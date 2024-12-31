c1275352c346ba59128e70364fe08c209c433c93
Ismael <henriqueisma999@gmail.com>
2024-12-31 19:41:00 -0300

    feat: add initial assets to main

---
+++ src/assets/index.ts
@@ -0,0 +1,31 @@
+import arrowDown from './arrow-down.svg';
+import arrowExplorer from './arrow-explorer.svg';
+import bgHero from './bg-hero.jpg';
+import btnAppleStore from './btn-apple-store.svg';
+import btnGooglePlay from './btn-google-play.svg';
+import iconCard from './icon-card.svg';
+import iconOptions from './icon-options.svg';
+import iconPhone from './icon-phone.svg';
+import iconSearch from './icon-search.svg';
+import iconSolutions from './icon-solutions.svg';
+import iconUser from './icon-user.svg';
+import logo from './logo.svg';
+import phone from './phone.png';
+import woman from './woman.png';
+
+export {
+    arrowDown,
+    arrowExplorer,
+    bgHero,
+    btnAppleStore,
+    btnGooglePlay,
+    iconCard,
+    iconOptions,
+    iconPhone,
+    iconSearch,
+    iconSolutions,
+    iconUser,
+    logo,
+    phone,
+    woman
+};
