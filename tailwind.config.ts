c5d5297d7fad971bea79eabea91aa1736b0ae45e
Ismael <henriqueisma999@gmail.com>
2025-01-01 12:43:43 -0300

    fix: correct content and extend configuration in tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,18 +3,21 @@ import type { Config } from 'tailwindcss';
 const config: Config = {
     content: [
         './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-        './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-        './src/app/**/*.{js,ts,jsx,tsx,mdx}'
+        './src/components/*.{js,ts,jsx,tsx,mdx}',
+        './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+        './src/**/*.{js,ts,jsx,tsx,mdx}'
     ],
     theme: {
         extend: {
-            'primary-blue': '#106EB0',
-            'primary-orange': '#EC7000',
-            'primry-gray': '#33303E',
-            'second-gray': '#4E4B59',
-            'gray-phone': '#F4F4F4',
-            'txt-gray': '#7A7786',
-            'opacity-gray': '#rgba(100,80,57,0.1)'
+            colors: {
+                'primary-blue': '#106EB0',
+                'primary-orange': '#EC7000',
+                'primry-gray': '#33303E',
+                'second-gray': '#4E4B59',
+                'gray-phone': '#F4F4F4',
+                'txt-gray': '#7A7786',
+                'opacity-gray': 'rgba(100,80,57,0.1)'
+            }
         }
     },
     plugins: []
